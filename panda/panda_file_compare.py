fcdc93beaeca4660aa8e5a0cbe3ba60794eb0b37
Anujj_Ssaxena <admirableanujj@gmail.com>
2023-03-22 01:52:26 -0400

    added third dataframe

--- panda/panda_file_compare.py
+++ panda/panda_file_compare.py
@@ -2,6 +2,11 @@ import os
 import pandas as pd
 import numpy as np
 import configparser
+import string
+
+
+def match_percent(x,y):
+    return x+y
 
 
 config = configparser.ConfigParser()
@@ -18,15 +23,32 @@ path_to_sample_data = path_to_sample_file['path']
 file1  = sample_file_name['name1']
 file2 = sample_file_name['name2']
 
-df1 = pd.read_excel(path_to_sample_data+file1, sheet_name=['Sheet1'])
-print(df1['Sheet1'])
-df2 = pd.read_excel(path_to_sample_data+file2, sheet_name=['Sheet1'])
-print(df2['Sheet1'])
+data1 = pd.read_excel(path_to_sample_data+file1, sheet_name=['Sheet1'])
+df1_sheet1 = data1['Sheet1']
+data2 = pd.read_excel(path_to_sample_data+file2, sheet_name=['Sheet1'])
+df2_sheet1 = data2['Sheet1']
+# df1 = pd.DataFrame.from_dict(data1, orient='index')
+# df2 = pd.DataFrame.from_dict(data2, orient='index')
 
-A = df1['Sheet1'].to_numpy()
-X = df2['Sheet1'].to_numpy()
+compare_df  = pd.DataFrame()
+# compare_df['A']=pd.Series([10,20,30],index=['a','b','c'])
+# compare_df["A"] = None
+# compare_df["A"] = np.where(df1_sheet1['A']+df2_sheet1['X'] >=20, 'True', 'False')
+r,c = df1_sheet1.shape
+np.full((r,c),np.nan)
+asciilower = list(string.ascii_lowercase)
+compare_df=pd.DataFrame(data=np.full((r,c),np.nan),index=asciilower[:c])
+compare_df["c"] = np.where(df1_sheet1['X']+df2_sheet1['A'] >=5, 10, 11)
+print(df1_sheet1)
+print(df2_sheet1)
+print(compare_df)
 
-print(A)
-print(X)
-print(np.matmul(A,X))
+
+
+
+# A = df1['Sheet1'].to_numpy()
+# X = df2['Sheet1'].to_numpy()
+# print(A)
+# print(X)
+# print(np.matmul(A,X))
 
