bbb7fdf6a3f8977bd0b8c468b4786431a29cb8f9
Saxena <ajs@iti-global.com>
2023-04-06 14:05:15 -0400

    Problem24

--- 24.py
+++ 24.py
@@ -21,92 +21,26 @@ given =  list(range(0,10))
 rank = 1000000 -1
 given = list(range(10))
 ans = []
-P1010 = int(permutation(10,10))
-print(f'P1010 = {P1010}')
-print(f'rank = {rank}||rank/P1010 = {rank/P1010}')
-print(f'given:{given}||len(given):{len(given)}||ans:{ans}')
-rank = rank - int(rank/P1010) * P1010
-print('=====================================================')
-
-P99  = int(permutation(9,9))
-print(f'P99 = {P99}')
-print(f'rank = {rank}||rank/P99 = {rank/P99}')
-ans.append(given.pop(int(rank/P99)))
-print(f'given:{given}||len(given):{len(given)}||ans:{ans} || ')
-rank = rank - int(rank/P99) * P99
-print('=====================================================')
-
-P88  = int(permutation(8,8))
-print(f'P88 = {P88}')
-print(f'rank = {rank}||rank/P88 = {rank/P88}')
-ans.append(given.pop(int(rank/P88)))
-print(f'given:{given}||len(given):{len(given)}||ans:{ans}')
-rank = rank - int(rank/P88) * P88
-print('=====================================================')
-
-P77  = int(permutation(7,7))
-print(f'P77 = {P77}')
-print(f'rank = {rank}||rank/P77 = {rank/P77}')
-ans.append(given.pop(int(rank/P77)))
-print(f'given:{given}||len(given):{len(given)}||ans:{ans}')
-rank = rank - int(rank/P77) * P77
-print('=====================================================')
-
-P66  = int(permutation(6,6))
-print(f'P66 = {P66}')
-print(f'rank = {rank}||rank/P66 = {rank/P66}')
-ans.append(given.pop(int(rank/P66)))
-print(f'given:{given}||len(given):{len(given)}||ans:{ans}')
-rank = rank - int(rank/P66) * P66
-print('=====================================================')
-
-P55  = int(permutation(5,5))
-print(f'P55 = {P55}')
-print(f'rank = {rank}||rank/P55 = {rank/P55}')
-ans.append(given.pop(int(rank/P55)))
-print(f'given:{given}||len(given):{len(given)}||ans:{ans}')
-rank = rank - int(rank/P55) * P55
-print('=====================================================')
-
-P44  = int(permutation(4,4))
-print(f'P44 = {P44}')
-print(f'rank = {rank}||rank/P44 = {rank/P44}')
-ans.append(given.pop(int(rank/P44)))
-print(f'given:{given}||len(given):{len(given)}||ans:{ans}')
-rank = rank - int(rank/P44) * P44
-print('=====================================================')
-
-P33  = int(permutation(3,3))
-print(f'P33 = {P33}')
-print(f'rank = {rank}||rank/P33 = {rank/P33}')
-ans.append(given.pop(int(rank/P33)))
-print(f'given:{given}||len(given):{len(given)}||ans:{ans}')
-rank = rank - int(rank/P33) * P33
-print('=====================================================')
-
-P22  = int(permutation(2,2))
-print(f'P22 = {P22}')
-print(f'rank = {rank}||rank/P22 = {rank/P22}')
-ans.append(given.pop(int(rank/P22)))
-print(f'given:{given}||len(given):{len(given)}||ans:{ans}')
-rank = rank - int(rank/P22) * P22
-print('=====================================================')
-
-P11  = int(permutation(1,1))
-print(f'P11 = {P11}')
-print(f'rank = {rank}||rank/P11 = {rank/P11}')
-ans.append(given.pop(int(rank/P11)))
-print(f'given:{given}||len(given):{len(given)}||ans:{ans}')
-rank = rank - int(rank/P11) * P11
-print('=====================================================')
-
-P00  = int(permutation(0,0))
-print(f'P00 = {P00}')
-print(f'rank = {rank}||rank/P00 = {int(rank/P00)}')
-ans.append(given.pop(int(rank/P00)))
-print(f'given:{given}||len(given):{len(given)}||ans:{ans}')
-rank = rank - int(rank/P00) * P00
-print('=====================================================')
+n = len(given)
+r = n
+while given and rank != 0:
+    
+    possible_permutation = int(permutation(n,r))
+    print(f'P{n}{r} = {possible_permutation}')
+    print(f'rank = {rank}||rank/P1010 = {rank/possible_permutation}')
+    print(f'given:{given}||len(given):{len(given)}||ans:{ans}')
+    temp_rank = rank - int(rank/possible_permutation) * possible_permutation
+    if temp_rank != rank:
+            ans.append(given.pop(int(rank/possible_permutation)))
+            rank = temp_rank
+            n = len(given) 
+            r = n
+            continue 
+    n = len(given) - 1
+    r = n
+    print('=====================================================')
+ans.extend(given)
+print(f'ans:{ans}')
 
 
 print(2783915460)
